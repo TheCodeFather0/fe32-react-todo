384967f5e00348be3b8bd34f6a93101601e748a9
TheCodeFather0 <raminmammadov056@gmail.com>
2024-11-16 19:33:33 +0400

    left component created

---
+++ src/Components/Left.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Left = () => {
+  return (
+    <div className="left bg-success">
+      <button className="btn btn-outline-light">Todos</button>
+      <button className="btn btn-outline-light">Add Todo</button>
+    </div>
+  );
+};
