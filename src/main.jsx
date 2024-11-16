1c0e9c7ead1d77de214b989117d500be172597e6
TheCodeFather0 <raminmammadov056@gmail.com>
2024-11-16 19:29:05 +0400

    added bootstrap cdn

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import "./index.css";
+import App from "./App.jsx";
+import "bootstrap/dist/css/bootstrap.css";
+import { createRoot } from "react-dom/client";
+
+createRoot(document.getElementById("root")).render(<App />);
