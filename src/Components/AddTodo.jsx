e86c0ffda423b0777dac024bd15e27d0de2e9d73
TheCodeFather0 <raminmammadov056@gmail.com>
2024-11-16 20:31:57 +0400

    add todo completed

--- src/Components/AddTodo.jsx
+++ src/Components/AddTodo.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useRef } from "react";
+import toast from "react-hot-toast";
 
-const AddTodo = () => {
-  return <div className="addTodo">addTodo</div>;
+const AddTodo = ({ setTodos, todos }) => {
+  const inpref = useRef();
+
+  const addNewTodo = (e) => {
+    e.preventDefault();
+    const date = new Date();
+    const data = {
+      id: date.getTime(),
+      todo: inpref.current.value,
+      isDone: false,
+    };
+    if (inpref.current.value) {
+      setTodos([data, ...todos]);
+      toast.success("Yeni todo elave edildi");
+      inpref.current.value = "";
+      inpref.current.focus();
+    } else {
+      toast.error("todo bos olmamalidir");
+    }
+  };
+  return (
+    <div className="addTodo bg-secondary">
+      <div className="col-6">
+        <form onSubmit={addNewTodo}>
+          <input
+            className="form-control p-3"
+            type="text"
+            placeholder="Todo"
+            ref={inpref}
+          />
+          <input
+            className="form-control p-3 mt-3"
+            type="submit"
+            value="Add Todo"
+          />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default AddTodo;
